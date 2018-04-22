54670a3eb2e3f1f5d5c6dd944bbc7da42e3f1311
Nenad Vujicic <nndprogramer@gmail.com>
2018-04-22 22:45:24 +0200

    Zadatak 1

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Animals</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Date of birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(animal, key) in animals" :key="key">
+          <td>{{ animal.species }}</td>
+          <td>{{ animal.name }}</td>
+          <td>{{ animal.dateOfBirth }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data() {
+    return {
+      animals: [
+        {
+          species: 'Dog',
+          name: 'Ben',
+          dateOfBirth: '12-12-2012'
+        },
+        {
+          species: 'Cat',
+          name: 'Cloe',
+          dateOfBirth: '05-05-2010'
+        },
+        {
+          species: 'Parot',
+          name: 'Jargo',
+          dateOfBirth: '24-02-2015'
+        },
+        {
+          species: 'Fish',
+          name: 'Nemo',
+          dateOfBirth: '08-08-2016'
+        },
+        {
+          species: 'Horse',
+          name: 'Thunder',
+          dateOfBirth: '12-09-2009'
+        }
+      ]
+    };
+  }
+};
+</script>
