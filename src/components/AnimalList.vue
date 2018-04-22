104d52003ae02d02cb89551fe90e545140eeb766
Nenad Vujicic <nndprogramer@gmail.com>
2018-04-22 23:01:52 +0200

    Zadatak 4

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -17,6 +17,7 @@
           <td>{{ animal.dateOfBirth ? animal.dateOfBirth : 'Nepoznat' }}</td>
           <td class="text-right">
             <button class="btn btn-danger btn-sm" @click="remove(animal)">Remove</button>
+            <button class="btn btn-default btn-sm" @click="moveToTop(animal)">Move to top</button>
           </td>
         </tr>
       </tbody>
@@ -62,6 +63,10 @@ export default {
     remove(animal) {
       const index = this.animals.indexOf(animal);
 		  this.animals.splice(index, 1);
+    },
+    moveToTop(animal) {
+      this.remove(animal);
+      this.animals.unshift(animal);
     }
   }
 };
